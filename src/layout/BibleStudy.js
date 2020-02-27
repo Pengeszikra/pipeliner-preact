58bcb087e26979223efeae333b3156b512a1033b
Peter Vivo <petervivo@bitbucket.org>
2020-02-27 23:06:26 +0100

    simple useEffect beat callbag

--- src/layout/BibleStudy.js
+++ src/layout/BibleStudy.js
@@ -6,36 +6,34 @@ import { kereses } from '../flow/szentirasApi';
 
 import useReducerActions from '../utils/useReducerActions';
 import {reducer, initialState, actions} from '../flow/bibleReducer';
-import { fromIter, forEach, pipe, filter } from 'callbag-basics';
+import { fromIter, forEach, pipe, filter, combine, merge, map } from 'callbag-basics';
 import { debounce } from 'callbag-debounce';
 import fromEvent from 'callbag-from-event';
 import mergeWith from 'callbag-merge-with';
 import fromFunction from 'callbag-from-function';
 import timer from 'callbag-timer';
+import  interval  from 'callbag-interval';
+
+let toShort = null;
+const treshold = 1000;
 
 export default () => {
   const {state, askAbout, answerReady } = useReducerActions(reducer, initialState, actions);
   const {answer, search} = state;
 
   const changeSearchInput = ({target:{value}}) => value |> askAbout;
-  const {source:watchSearch, emitter: checkSearch} = fromFunction(p => p);
- 
-  useLayoutEffect(() => {
-    /*
-    if (search.length > 3) {
-      search |> kereses(json => JSON.stringify(json, null, 2) |> answerReady );
-    } 
-    */
-    checkSearch(search);
-  }, [search]);
+  const apiSearch = searchText => searchText |> kereses(json => JSON.stringify(json, null, 2) |> answerReady );
 
-  watchSearch |> debounce(500)|> filter(search => search.length > 2) |> forEach(console.log);
+  useEffect(() => {
+    clearTimeout(toShort);
+    toShort = setTimeout(() => search.length > 2 && apiSearch(search), treshold); 
+  }, [search])
   
   return (
     <pre>{`
     -- Bible Study --
 
-     How speed up javascript development process?
+     simple useEffect beat callbag
     
     `} 
     <a href="https://szentiras.hu/api" target="_base">szentiras.hu/api</a>
